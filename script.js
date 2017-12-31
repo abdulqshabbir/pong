9e339bde6e6f4350bba3eb0a3784fa9c5b562d6e
Abdul Shabbir <abdulqshabbir@gmail.com>
2017-12-31 22:26:52 +0300

    bug which was causing the bottom half of the player paddle to not register is fixed.  Computer and player paddles are working normally.  The player paddle now has the functionality of deflecting the ball at an angle

--- script.js
+++ script.js
@@ -6,34 +6,33 @@ const displayComputerScore = document.getElementById('computerScore');
 let playerScore = 0;
 let computerScore = 0;
 
-const CANVAS_WIDTH = 400;
-const CANVAS_HEIGHT = 300;
-//SCREEN_EDGE is how far the top-left corner of the canvas is from the viewport
+window.onload = function() {// wait for window to load
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const CANVAS_TOP = canvas.getBoundingClientRect().top;
+const CANVAS_BOTTOM = canvas.getBoundingClientRect().bottom;
+const CANVAS_LEFT = canvas.getBoundingClientRect().left;
+const CANVAS_WIDTH = canvas.width;
+const CANVAS_HEIGHT = canvas.height;
 const SCREEN_EDGE = 0;
 const PADDLE_WIDTH = 10;
 const PADDLE_HEIGHT = 70;
 const BALL_RADIUS = 7;
 const FRAMES_PER_SECOND = 35;
-const HEIGHT_OF_SCORE_DIVS = 59.2;
 let mouseX = 0;
 let mouseY = 0;
-let ballXPosition = SCREEN_EDGE + CANVAS_WIDTH/2;//center vertically
-let ballYPosition = SCREEN_EDGE + CANVAS_HEIGHT/2;//center horizontally
+let ballXPosition = CANVAS_TOP + CANVAS_WIDTH/2;//center vertically
+let ballYPosition = CANVAS_LEFT + CANVAS_HEIGHT/2;//center horizontally
 let ballXVelocity = 8;
 let ballYVelocity = 5;
-let rightPaddleCenter = SCREEN_EDGE + CANVAS_HEIGHT/2;
+let rightPaddleCenter = CANVAS_TOP + CANVAS_HEIGHT/2;
 let throwBallInPlayersDirection = true;
 let numberOfRallies = 0; //used to keep track of how fast to move ball
-
-window.onload = function() {// wait for window to load
+//move player paddle on mouse move
 
 // --------------------Event Listeners ---------------------------//
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-const CANVAS_TOP = canvas.getBoundingClientRect().top;
-const CANVAS_BOTTOM = canvas.getBoundingClientRect().bottom;
 
-//move player paddle on mouse move
 window.addEventListener('mousemove', (event) => {
   mouseX = event.x;
   mouseY = event.y;
@@ -46,9 +45,9 @@ setInterval(drawObjects, 1000/FRAMES_PER_SECOND); //animate!
 
 function drawObjects() {
   //clear existing canvas
-  context.clearRect(SCREEN_EDGE, SCREEN_EDGE, 600, 500);
+  context.clearRect(CANVAS_LEFT, CANVAS_TOP, CANVAS_WIDTH, CANVAS_HEIGHT);
   //draw pong background
-  drawRectangle(SCREEN_EDGE, SCREEN_EDGE, CANVAS_WIDTH,
+  drawRectangle(CANVAS_LEFT, CANVAS_TOP, CANVAS_WIDTH,
     CANVAS_HEIGHT, 'black');
 
   movePlayerPaddle(mouseX, mouseY);
@@ -57,22 +56,23 @@ function drawObjects() {
 }
 
 function movePlayerPaddle(mouseX, mouseY) {
-  let leftPaddlePosition = mouseY;
+  let centerOfLeftPaddle = mouseY;
+  let topOfLeftPaddle = mouseY - PADDLE_HEIGHT/2;
+  let bottomOfLeftPaddle = mouseY + PADDLE_HEIGHT/2;
 
-  if(leftPaddlePosition <= SCREEN_EDGE + PADDLE_HEIGHT/2) {
+  if(topOfLeftPaddle <= CANVAS_TOP) {
     //if mouse leaves canvas from top, fix left paddle to top of canvas
-    drawRectangle(SCREEN_EDGE, SCREEN_EDGE, PADDLE_WIDTH,
+    drawRectangle(CANVAS_LEFT, CANVAS_TOP, PADDLE_WIDTH,
       PADDLE_HEIGHT, 'white');
   }
-  else if(leftPaddlePosition >= SCREEN_EDGE + CANVAS_HEIGHT
-    - PADDLE_HEIGHT/2) {
+  else if(bottomOfLeftPaddle >= CANVAS_BOTTOM) {
     //if mouse leaves canvas from bottom, fix left paddle to bottom of canvas
-    drawRectangle(SCREEN_EDGE, SCREEN_EDGE + CANVAS_HEIGHT -
+    drawRectangle(CANVAS_LEFT, CANVAS_TOP + CANVAS_HEIGHT -
       PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
   }
   else{
     //move paddle normally
-    drawRectangle(SCREEN_EDGE, mouseY - CANVAS_TOP - PADDLE_HEIGHT/2,
+    drawRectangle(CANVAS_LEFT, mouseY - CANVAS_TOP - PADDLE_HEIGHT/2,
       PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
   }
 }
@@ -95,7 +95,7 @@ function moveComputerPaddle() {
   }
   //draw right paddle after its positions is updated
 
-  drawRectangle(SCREEN_EDGE + CANVAS_WIDTH - PADDLE_WIDTH,
+  drawRectangle(CANVAS_LEFT + CANVAS_WIDTH - PADDLE_WIDTH,
       rightPaddleCenter - PADDLE_HEIGHT/2, PADDLE_WIDTH,
       PADDLE_HEIGHT, 'white');
 }
@@ -108,7 +108,7 @@ function moveBall() {
   let topOfRightPaddle = rightPaddleCenter - PADDLE_HEIGHT/2;
   let bottomOfRightPaddle = rightPaddleCenter + PADDLE_HEIGHT/2;
 
-  if(ballXPosition >= SCREEN_EDGE + CANVAS_WIDTH) { //right edge
+  if(ballXPosition >= CANVAS_LEFT + CANVAS_WIDTH) { //right edge
     if(ballYPosition >= topOfRightPaddle &&
         ballYPosition <= bottomOfRightPaddle) {
           //ball hits computer paddle!
@@ -121,7 +121,7 @@ function moveBall() {
       updateScore('player');
     }
   }
-  if (ballXPosition <= SCREEN_EDGE) {//left edge
+  if (ballXPosition <= CANVAS_LEFT) {//left edge
     if(ballYPosition >= topOfLeftPaddle && ballYPosition <= bottomOfLeftPaddle) {
       //ball hits player paddle!
       ballXVelocity = -ballXVelocity;
@@ -136,11 +136,11 @@ function moveBall() {
       updateScore('computer');
     }
   }
-  if (ballYPosition >= SCREEN_EDGE + CANVAS_HEIGHT) {
+  if (ballYPosition >= CANVAS_TOP + CANVAS_HEIGHT) {
     //bounce off of bottom edge
     ballYVelocity = -ballYVelocity;
   }
-  if (ballYPosition <= SCREEN_EDGE) {
+  if (ballYPosition <= CANVAS_TOP) {
     //bounce off of top edge
     ballYVelocity = -ballYVelocity;
   }
@@ -162,7 +162,7 @@ function resetBall() {
   ballYPosition = SCREEN_EDGE + CANVAS_HEIGHT/2;
 
   if(throwBallInPlayersDirection === true) {
-    ballXVelocity = -8
+    ballXVelocity = -8;
   }
   else {
     ballXVelocity = 8;
